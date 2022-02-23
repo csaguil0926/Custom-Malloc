f2b2878e2a0b31e1e19ddf4c8d82c6582ec56ce2
Carolette A. Saguil <cas699@ilab2.cs.rutgers.edu>
2022-02-23 17:23:53 -0500

    Added 3 errors and made it so user knows what file and line error is happening
    Added errors that deal with wrong pointer, user requesting more than memsize, and user running out of mem, fixed return null, print statements now show the file and line the error is happening in

--- src/errors.h
+++ src/errors.h
@@ -2,9 +2,12 @@
 #define _ERRORS_H
 
 //prototypes of errors from errors.c
-void doubleFree();
-void segFault();
-void heapBufferOverflow();
-void memoryLeak();
+void doubleFree(char* file, int line);
+void segFault(char* file, int line);
+void heapBufferOverflow(char* file, int line);
+void memoryLeak(char* file, int line);
+void wrongPointer(char* file, int line);
+void tooMuchMem(int MEMSIZE, char* file, int line);
+void noMoreMem(char* file, int line);
 
 #endif
